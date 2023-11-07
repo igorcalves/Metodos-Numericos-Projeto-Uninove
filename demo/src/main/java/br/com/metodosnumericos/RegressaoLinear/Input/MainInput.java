bb8e0989b78cf1b06fa3d032f731e8d4acb34dca
igor alves <igor193@live.com>
2023-11-07 05:10:00 +0000

    input complete first Formula

--- demo/src/main/java/br/com/metodosnumericos/RegressaoLinear/Input/MainInput.java
+++ demo/src/main/java/br/com/metodosnumericos/RegressaoLinear/Input/MainInput.java
@@ -12,44 +12,53 @@ public class MainInput {
 
     private Scanner sc = new Scanner(System.in);
 
-    public void All(){
+    public void All() {
         Resolving resolving = new Resolving();
-        List<Double> listX =getList('X');
-        resolving.sumAll(listX, getList('Y'));
+        List<Double> listX = getList('X', -1);
+        resolving.sumAll(listX, getList('Y', listX.size()));
         resolving.firstFormula(listX.size());
         System.out.println(resolving.resultFirstFOrmula());
     }
 
-
-     private List<Double> getList(char option) {
+    private List<Double> getList(char option, int size) {
         List<Double> genericList = new ArrayList<>();
         String condition = "";
         System.out.print(Messages.rules);
+        int cont = 0;
         while (!condition.equalsIgnoreCase("proximo")) {
-            System.out.print("Digite O valor de"+option+": ");
+            System.out.print("Digite O valor de " + option + ": ");
             String valueNumeric = sc.nextLine();
-            if(isValidDoubleNumeric(valueNumeric)){
-                genericList.add(Double.parseDouble(valueNumeric));
-
-            }else {
-                condition = valueNumeric;
-                System.out.print(Messages.errorMessage);
+            condition = valueNumeric;
+            if(! valueNumeric.equalsIgnoreCase("proximo")){
+                if (option == 'X') {
+                    if (isValidDoubleNumeric(valueNumeric)) {
+                        genericList.add(Double.parseDouble(valueNumeric));
+                    }else System.out.print(Messages.errorMessage);
+                }
+                if (option == 'Y') {
+                    if (isValidDoubleNumeric(valueNumeric)) {
+                        genericList.add(Double.parseDouble(valueNumeric));
+                        cont++;
+                        if (cont == size) {
+                            return genericList;
+                        }
+                    }else System.out.print(Messages.errorMessage);
+                }
+    
+            }
             }
             
-        }
+
         return genericList;
-     }
+    }
 
-
-     public static boolean isValidDoubleNumeric(String numeric){
+    public static boolean isValidDoubleNumeric(String numeric) {
         try {
-             Double.parseDouble(numeric);
-             return true;
-         } catch (NumberFormatException e) {
-             return false;
-         }
-     }
-
-
+            Double.parseDouble(numeric);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 }
--- demo/src/main/java/br/com/metodosnumericos/RegressaoLinear/Util/Messages.java
+++ demo/src/main/java/br/com/metodosnumericos/RegressaoLinear/Util/Messages.java
@@ -4,7 +4,8 @@ public class Messages {
     public static String rules = 
     "O valor deve ser um numero\n"
     +"Voce deve digitar os valores de X e Y\n"
-    +"O programa começa recebendo os valores de X assim que digitar todos valores escreva ['Proximo'] para digitar os valores de Y\n";
+    +"O programa começa recebendo os valores de X assim que digitar todos valores escreva ['Proximo'] para digitar os valores de Y\n"
+    +"A quantidade de Valores de X e Y devem ser iguais\n";
     public static String errorMessage = "Valor Invalido Por Favor digite Novamente\n";
     
 }
